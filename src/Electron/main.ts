8c29beb9a7cd97ccfc4238b36eeb1f963556d035
mahmoud26796 <mahmoudmrm96@gmail.com>
2026-03-06 03:09:16 +0200

    finished the logic for checking the dev env for woking on local host with local dev

--- src/Electron/main.ts
+++ src/Electron/main.ts
@@ -1,7 +1,14 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { isDev } from "./util.js";
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  // if in local dev env we are using the web
+  if(isDev()) {
+    mainWindow.loadURL("http://localhost:5173");
+  } else {
+    // else means in production we use the desktop app
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
 });
